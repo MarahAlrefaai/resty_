b47b2c7e63df276dca0a3f9f8f9b85a350d22764
MarahAlrefaai <marahalrefaai17@gmail.com>
2022-05-20 23:40:19 +0300

    done with superagent to fetch data lab26 and using the use effect and use state hooks

--- src/component/form/form.js
+++ src/component/form/form.js
@@ -1,23 +1,56 @@
-import React  from "react";
+import React,{ useState } from "react";
 import "./form.scss";
-function Form(props) {  
- 
-    
+
+function Form(props) {
+  //-----------------------------------------
+ const [url , setUrl]=useState(); 
+ const [method , setMethod]=useState("Get");
+ const [body, setBody] = useState(null);
+
+ const handleURL = (event) => {
+   //to save the url in each time i change it 
+    event.preventDefault();
+    setUrl(event.target.value);
+
+  } 
+  const handleSubmit = event => {
+    //to save the data come from the form  in each time i change it 
+    event.preventDefault();
+    const formData = {
+      method: method,
+      url: url,
+      body: null
+    };
+    if (body) 
+    {formData.body = body;}
+    props.handleApiCall(formData);//send the data to this function to use it in axios 
+  }
+ const handleMethod = (event) => {
+    event.preventDefault();
+    setMethod(event.target.id);
+  }
+  const handleBody = (event) => {
+    event.preventDefault();
+    setBody(event.target.value);
+  }
+  
+  
+  //-----------------------------------------
     return ( 
-        <form className="form" onSubmit={props.handleSubmit}>
+        <form className="form" onSubmit={handleSubmit}>
             <div >
-                <input className="input" type="text" name="url" placeholder="http://api.url.here"  />
+                <input onChange={handleURL} className="input" type="text" name="url" placeholder="http://api.url.here"  />
                
-                <button type="submit"  > Go!  </button>
+                <button type="submit"  onClick={!props.isloading ? props.handleClick : null}  > {method}  </button>
             </div>
             <div className="methods">
-                <button className="method" id="method" name ="method" >GET</button>
-                <button  className="method" id="method" name = "method" >POST</button>
-                <button  className="method" id="method" name = "method" >PUT</button>
-                <button  className="method" id="method" name = "method" >DELETE</button>
+                <button onClick={handleMethod} className="method" id="GET" name ="GET" >GET</button>
+                <button onClick={handleMethod} className="method" id="POST" name = "POST" >POST</button>
+                <button onClick={handleMethod} className="method" id="PUT" name = "PUT" >PUT</button>
+                <button onClick={handleMethod} className="method" id="DELETE" name = "DELETE" >DELETE</button>
 
             </div>
-            <textarea name="body"  id="textdata"defaultValue="{}"/>
+            <textarea name="body" onChange={handleBody} id="textdata"defaultValue="{}"/>
         </form>)
 }
 export default Form;
--- src/component/home/home.js
+++ src/component/home/home.js
@@ -1,33 +1,43 @@
-import React  from 'react'; // useeffect is to loading use state is to get the data 
+import React, { useState , useEffect}  from 'react'; // useeffect is to loading use state is to get the data 
 import Form from '../form/form.js';
 import Result from '../result/result.js';
+import superagent from 'superagent';// to fetch Data from Api 
+
 
   export default function App (props) {  
 
+    const [data, setData] = useState(null);
+    const [header, setHeader] = useState({});
+    const [isloading, setLoading] = useState(false);
+    const [requestParams, setRequest] = useState({});
     
-    let formData={
-      method:null,
-      url:null,
-      body:null
-    }
     //============================================
-    const handleSubmit = (event) => {
-      //to save the data come from the form  in each time i change it 
-      event.preventDefault();
-      
-       formData.method= event.target.method.value;
-       formData.url=event.target.url.value;
-       formData.body=`{Name : "marah",age :26 ,major : "cs}`;
-       console.log("home===> "+formData.url)
-      console.log(formData.body)
-      console.log(formData.method)
-      }
+    const handleApiCall = async (requestParams) => {
+      //requestParams ==> data that we take it from the form 
+      setRequest(requestParams);
+      // superagent must take method  in lowercase 
+      const response = await superagent[requestParams.method.toLowerCase()](requestParams.url, (requestParams.body) ? (requestParams.body) : null);    
+      setHeader( {
+        headers: response.headers
+      });
+      setData({
+        results: response.body
+      });
+    }
     //==================================
-    
+    const handleClick = () => setLoading(true);
+    useEffect(() => {
+        if (isloading) {
+        loading().then(() => {
+                setLoading(false);
+            });}
+    }, [isloading]);
+//========================================
+const loading = () => {return new Promise((resolve) => setTimeout(resolve, 1800));}
     return (
       <>
-        <Form  handleSubmit={handleSubmit}  />
-     <Result   data={formData}   />
+        <Form handleClick={handleClick} handleApiCall={handleApiCall}  />
+     <Result loading={isloading}  data={data}  url={requestParams.url} header={header}  method={requestParams.method} />
       </>
     );
   }
